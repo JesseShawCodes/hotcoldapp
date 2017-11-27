3014b3e5c6fc548d86c2b0932222282fd898e598
Jesse Shaw <jdshaw1987@gmail.com>
2017-11-27 17:45:51 -0500

    Removed un-needed components

--- src/components/header1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function topbar() {
-    return (
-        <ul className="character-list" aria-live="polite">
-            "testing"
-        </ul>
-    );
-}
